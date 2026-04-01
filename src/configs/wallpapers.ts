9e0622d5ed12389a9ba8a68f655231d98364434e
Aidan Botha <aidanbotha15@gmail.com>
2026-04-01 16:24:27 +0200

    file rename

--- src/configs/wallpapers.ts
+++ src/configs/wallpapers.ts
@@ -1,8 +1,8 @@
 import type { WallpaperData } from "~/types";
 
 const wallpapers: WallpaperData = {
-  day: "public/img/ui/dmitry-spravko-b8PZOwi8iPA-unsplash.jpg",
-  night: "public/img/ui/dmitry-spravko-b8PZOwi8iPA-unsplash.jpg"
+  day: "/img/ui/dmitry-spravko-b8PZOwi8iPA-unsplash.jpg",
+  night: "/img/ui/dmitry-spravko-b8PZOwi8iPA-unsplash.jpg"
 };
 
 export default wallpapers;
